5db94631e772d2441e9049a7d027669bbd47ff14
Amida Fombutu <amida.fombutu@gmail.com>
2022-11-22 11:07:46 -0600

    posts

--- src/main/java/com/example/haskellsprinblog/PostController.java
+++ src/main/java/com/example/haskellsprinblog/PostController.java
@@ -1,25 +1,37 @@
 package com.example.haskellsprinblog;
 
+import models.Post;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
 
     @RequestMapping(method = RequestMethod.GET,path= "/posts")
-    @ResponseBody
-    public String posts(){
-        return "posts index page";
+    public String posts(Model model){
+        ArrayList<Post> post = new ArrayList<>();
+        post.add(new Post("iphone","Brand new a a cheap price"));
+        post.add(new Post("55inch Sansung Tv","Brand new a a cheap price"));
+
+        model.addAttribute("posts",post);
+
+        return "posts/index";
     }
 
 
     @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
-        @ResponseBody
-        public String byId(@PathVariable int id){
-            return "view an individual post";
+
+        public String byId(@PathVariable int id, Model model){
+         Post post = new Post(id,"first post","first time studying spring");
+
+model.addAttribute("post", post);
+        return "posts/show";
         }
 
 
